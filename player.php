236c157b66081d677fd3a967abfd4da601cf7f71
ale804 <71037797+ale804@users.noreply.github.com>
2024-04-13 21:29:28 -0400

    Create player.php

---
+++ player.php
@@ -0,0 +1,7 @@
+
+
+{ "radio": [ { "radio_genre": "Suena en Vivo 3", "radio_url": "https://streaming01.radiosenlinea.com.ar:8138", "radio_image_url": "https://i.ibb.co/5Wx0T2j/logocuadro.png", "background_image_url": "https://i.ibb.co/5hTwV4C/fondoapp.png",	 "blur_radio_background": "false", "song_metadata": "true", "image_album_art": "true",	 "image_album_art_dynamic_background": "false",	 "auto_play": "true",	 "chatbox_url": "#" } ],
+"panel": [ { "fondo_url": "#00000000", "about_color": "#00000000", "statusBar_color": "#5665EF", "navigationBar_color": "#5665EF",
+"logo_url": "https://i.ibb.co/5Wx0T2j/logocuadro.png", "menulogo_url": "https://i.ibb.co/0sM02pk/locutores.png",
+"radio_name": "Suena en Vivo 3", "dominio_url": "https://google.com", "copyright_url": "Copyright © 2024 Team Soft Digital Todos los derechos reservados.",
+"web_url": "https://google.com", "whatsapp_url": "541130964572",	 "call_url": "541130964572", "fanpage_url": "teamsoftdigital",	 "tiktok_url": "https://suenaenvivo3.teamsoftdigitalmundial.com/tv/",	 "twitter_url": "https://suenaenvivo3.teamsoftdigitalmundial.com/chat/" } ], "settings": [ { "app_status": "2.3.1", "onesignal_app_id": "00000000-0000-0000-0000-000000000000", "fcm_notification_topic": "your_single_radio_topic", "privacy_policy_url": "https://sites.google.com/view/poltica-de-privicidad/p%C3%A1gina-principal",	 "more_apps_url": "https://play.google.com/store/apps/dev?id=6788668719470215209",	 "redirect_url": "" } ]}
