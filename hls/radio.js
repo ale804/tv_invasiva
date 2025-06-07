4b91f7470554761d9ef82cd0a2a67d991c0b292e
ale804 <71037797+ale804@users.noreply.github.com>
2025-06-07 01:15:13 -0300

    Delete hls/radio.js

--- hls/radio.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-    const streamUrl = "https://usa14.fastcast4u.com/proxy/invasiva?mp=/1"; // Cambia esto cuando necesites actualizar el servidor
-    return {
-        statusCode: 302,
-        headers: {
-            "Location": streamUrl
-        }
-    };
-};
